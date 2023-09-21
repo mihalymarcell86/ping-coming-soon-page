e1b697cbb5642eb0458e04781180d024994cb359
Marcell Mihály <mihalymarcell86@gmail.com>
2023-09-21 18:42:08 +0100

    form input validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,55 @@
 import style from "../styles/Form.module.scss";
 
+import { useState } from "react";
+import { isEmpty, isEmail } from "validator";
+
 export default function Form() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+  const [touched, setTouched] = useState(false);
+
+  function validate(string) {
+    if (isEmpty(string) || !isEmail(string))
+      setError("Please provide a valid email address");
+    else setError(null);
+  }
+
+  function handleChange({ target: { value: string } }) {
+    setEmail(string);
+    if (touched) validate(string);
+  }
+
+  function handleBlur({ target: { value: string } }) {
+    setTouched(true);
+    validate(string);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setEmail("");
+    setError(null);
+    setTouched(false);
+  }
+
   return (
-    <form className={style.form} noValidate>
+    <form className={style.form} onSubmit={handleSubmit} noValidate>
       <input
         type="email"
         placeholder="Your email address&hellip;"
         autoComplete="email"
-        className={style.input}
+        className={`${style.input} ${error ? style.error : ""}`}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={email}
       />
-      <button className={style.button}>Notify Me</button>
+      {error && <p className={style.error_message}>{error}</p>}
+      <button
+        className={style.button}
+        onPointerUp={() => blur()}
+        disabled={error}
+      >
+        Notify Me
+      </button>
     </form>
   );
 }
